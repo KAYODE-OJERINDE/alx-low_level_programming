1975408bc9b71204f64e2c2a7029c75bdfb96cbc
Kayode Ojerinde <danieloluwakayode@gmail.com>
2023-02-18 03:35:27 -0800

    numbers commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,14 +3,14 @@
  * main -Entry point
  *
  * Return: Always 0 (success)
- */
+*/
 
 int main(void)
 {
 	int a;
 
 	for (a = 0; a < 10; a++)
-		putchar(a);
+		printf("%d", a);
 	putchar('\n');
 	return (0);
 }
