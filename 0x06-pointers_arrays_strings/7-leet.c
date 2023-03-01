56fb41fe2919ef79e25d5e2cca20964aa6cd45c8
Kayode Ojerinde <danieloluwakayode@gmail.com>
2023-03-01 10:37:39 -0800

    Task 7-encoding_string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * leet - This function encodes strings into 1337
+ * @s: This is the string to encode
+ * Return: pointer of s
+ */
+
+char *leet(char *s)
+{
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (a[j] == s[i])
+				s[i] = b[j];
+		}
+	}
+	return (s);
+}
